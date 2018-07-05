3a04e1eb8909c6d468c36f5f187c07218e541af5
leocavalcante <lc@leocavalcante.com>
2018-07-05 20:18:51 -0300

    Exporting OpenUrlAction

--- lib/facebook_canvas.dart
+++ lib/facebook_canvas.dart
@@ -4,5 +4,6 @@ export 'button.dart';
 export 'canvas.dart';
 export 'src/body_element.dart';
 export 'src/inline_style.dart';
+export 'src/open_url_action.dart';
 export 'src/rich_text.dart';
 export 'text.dart';
